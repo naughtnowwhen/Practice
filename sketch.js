53f4ca368488dfa90006654a70c1e936387a7d0d
Peter <naughtnowwhen@gmail.com>
2018-07-29 13:41:19 -0700

    two bubble objects

--- sketch.js
+++ sketch.js
@@ -9,10 +9,30 @@ var bubble = {
     ellipse(this.x, this.y, 30, 30);
   },
   move: function () {
+
+    // very cool, by having x and y tend towards a more random number the bubble slowly moves off screen. It be cool to have a bunch of bubble objects and set a seeker value so that all the bubbles start off randomly but slowly move towards that seeker value
     this.x = this.x + random(-1,1);
     this.y = this.y + random(-1,1);
   }
 };
+
+var bubble1 = {
+    x:210,
+    y:190,
+  
+  
+    display: function () {
+      stroke(255);
+      noFill();
+      ellipse(this.x, this.y, 30, 30);
+    },
+    move: function () {
+  
+      // very cool, by having x and y tend towards a more random number the bubble slowly moves off screen. It be cool to have a bunch of bubble objects and set a seeker value so that all the bubbles start off randomly but slowly move towards that seeker value
+      this.x = this.x + random(-1,1);
+      this.y = this.y + random(-1,1);
+    }
+  };
 
 function setup () {
   createCanvas(550, 350);
@@ -22,4 +42,6 @@ function draw () {
   background(10);
   bubble.move();
   bubble.display();
+  bubble1.move();
+  bubble1.display();
 }
