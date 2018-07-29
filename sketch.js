29f889d6a6de0f84b5c2b5f443b36e664cff917a
Peter <naughtnowwhen@gmail.com>
2018-07-29 13:54:56 -0700

    bubble in a bubbles array, for loop that display at index i of bubbles length

--- sketch.js
+++ sketch.js
@@ -1,47 +1,34 @@
-var bubble = {
-  x:200,
-  y:180,
+var bubbles = [];
 
 
-  display: function () {
-    stroke(255);
-    noFill();
-    ellipse(this.x, this.y, 30, 30);
-  },
-  move: function () {
 
-    // very cool, by having x and y tend towards a more random number the bubble slowly moves off screen. It be cool to have a bunch of bubble objects and set a seeker value so that all the bubbles start off randomly but slowly move towards that seeker value
-    this.x = this.x + random(-1,1);
-    this.y = this.y + random(-1,1);
-  }
-};
+function setup () {
+  createCanvas(550, 350);
+  bubbles[0] = {
+    x:200,
+    y:180,
 
-var bubble1 = {
-    x:210,
-    y:190,
-  
-  
+
     display: function () {
       stroke(255);
       noFill();
       ellipse(this.x, this.y, 30, 30);
     },
     move: function () {
-  
+
       // very cool, by having x and y tend towards a more random number the bubble slowly moves off screen. It be cool to have a bunch of bubble objects and set a seeker value so that all the bubbles start off randomly but slowly move towards that seeker value
       this.x = this.x + random(-1,1);
       this.y = this.y + random(-1,1);
     }
   };
+  console.log(bubbles);
 
-function setup () {
-  createCanvas(550, 350);
 }
 
 function draw () {
   background(10);
-  bubble.move();
-  bubble.display();
-  bubble1.move();
-  bubble1.display();
+for (var i = 0; i < bubbles.length; i ++) {
+  bubbles[i].move();
+  bubbles[i].display();
+}
 }
